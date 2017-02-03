5cb0347d628378b4425e5cc5058eea11f504e14c
Eric Scuccimarra <skooch@gmail.com>
2017-02-03 14:36:37 +0100

    Making it into a package

---
+++ src/BackupFile.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Escuccim\S3Backup;
+
+use Illuminate\Console\Command;
+use Aws\Laravel\AwsFacade as AWS;
+
+class BackupFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * file = name of file to upload
+     * dest = path/key to upload file to
+     * path = path to file to upload, relative to public directory
+     *
+     * @var string
+     */
+    protected $signature = 'backup:db {file} {--path=/} {--dest=/}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Uploads files to Amazon S3';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+    	$file = $this->argument('file');
+    	$destination = $this->option('dest');
+    	$path = $this->option('path');
+    	
+    	$s3 = AWS::createClient('s3');
+    	
+    	$s3->putObject(array(
+    			'Bucket'     => env('AWS_BUCKET'),
+    			'Key'        => $destination . '/' . $file,
+    			'SourceFile' => public_path() . '/' . $path . '/' . $file,
+    	));
+    }
+}
---
+++ src/S3BackupServiceProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Escuccim\S3Backup;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * This is the service provider.
+ *
+ * Place the line below in the providers array inside app/config/app.php
+ * <code>'JeroenG\Packager\PackagerServiceProvider',</code>
+ *
+ * @package Packager
+ * @author JeroenG
+ * 
+ **/
+class S3BackupServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * The console commands.
+     *
+     * @var bool
+     */
+    protected $commands = [
+        Escuccim\S3Backup\BackupFile::class,
+    ];
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Not really anything to boot.
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands($this->commands);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['s3backup'];
+    }
+}
